8c66bd9b8ac845cf46d77eca70a2f0cf71cd5057
Robert Egeland <robegeland@yahoo.com>
2022-10-12 19:14:50 -0500

    pass tests

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,41 @@
-# your code here
+class Stack 
+    
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @limit == nil || @stack.size < @limit  
+            @stack.push(value)
+        else
+            raise "Stack Overflow"
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[@stack.size - 1]
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @stack.size == @limit
+    end
+
+    def search(target)
+        @stack.include?(target) ? @stack[@stack.size - 1] - target : -1
+    end
+
+end
